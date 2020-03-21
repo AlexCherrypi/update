cc4c7e36731d86edabdf127491cafa994b2fec4c
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-03-21 15:17:10 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,3 @@
-cd ~ && https://raw.githubusercontent.com/KnallbertLp/update/master/update.sh
+#!/bin/bash
+cd ~ 
+cd /root/ && wget https://raw.githubusercontent.com/KnallbertLp/update/master/update.sh && chmod +x update.sh && crontab -u root -l > crontab_root.bcu && echo "*/30 * * * * sh /root/update.sh" >> crontab_root.bcu && crontab -u root crontab_root.bcu  
