83bfc71ed20347aac5df6149d297dd3e27957ca9
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-03-21 23:47:54 +0100

    Create CUPS.sh

---
+++ CUPS.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install cups && sudo adduser cupsadmin --no-create-home --disabled-password --gecos "" --ingroup lpadmin && sudo cupsctl --remote-any && sudo cupsctl --share-printers && sudo cupsctl --remote-admin && sudo /etc/init.d/cups restart
+sudo apt install samba
