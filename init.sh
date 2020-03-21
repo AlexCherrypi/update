af0ee732c44c091fecbf7a0da16216637e3882e2
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-03-21 23:49:57 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt update -y && apt full-upgrade -y && apt autoremove -y
 cd /root/ && wget https://raw.githubusercontent.com/KnallbertLp/update/master/update.sh && chmod +x update.sh && crontab -u root -l > crontab_root.bcu; echo "*/30 * * * * sh /root/update.sh" >> crontab_root.bcu && crontab -u root crontab_root.bcu && rm crontab_root.bcu 
+cd ~ && wget https://raw.githubusercontent.com/KnallbertLp/update/master/CUPS.sh && chmod +x CUPS.sh && sudo sh CUPS.sh && cd ~ && sudo rm CUPS.sh
