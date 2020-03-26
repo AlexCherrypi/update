44a71912e7a2d91e6ef69a8a8a41bbf9e031474c
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-03-26 18:18:36 +0100

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y && sudo apt install samba -y && sudo groupadd samba && sudo adduser sambaadmin --disabled-login --no-create-home --disabled-password --gecos "" --ingroup samba &&  sudo smbpasswd -a sambaadmin
